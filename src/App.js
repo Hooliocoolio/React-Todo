eb8bdf19da74ccc5941062953e375d624bf3d6e5
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-06-03 04:55:43 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+import TodoList from './components/TodoList';
+
+
+class App extends  Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Enter Your Next Todo',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Clicking on a task will mark it completed',
+          id: 1528817077287,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+
+  addTodo = e => {
+    e.preventDefault();
+    const todos = this.state.todos.slice();
+    todos.push({ task: this.state.todo, completed: false, id: Date.now() });
+    this.setState({ todos, todo: '' });
+  };
+
+  changeTodo = e => this.setState({ [e.target.name]: e.target.value });
+
+  toggleTodoComplete = id => {
+    let todos = this.state.todos.slice();
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    this.setState({ todos });
+  };
+
+  clearCompletedTodos = e => {
+    e.preventDefault();
+    let todos = this.state.todos.slice();
+    todos = todos.filter(todo => !todo.completed);
+    this.setState({ todos });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+      <h1>Welcome to your Todo List</h1>
+        <TodoList
+          className="todolist"
+          handleToggleComplete={this.toggleTodoComplete}
+          todos={this.state.todos}
+        />
+          <TodoForm 
+          className="form"
+          value={this.state.todo}
+          handleTodoChange={this.changeTodo}
+          handleAddTodo={this.addTodo}
+          handleClearTodos={this.clearCompletedTodos}
+        /> 
       </div>
     );
   }
